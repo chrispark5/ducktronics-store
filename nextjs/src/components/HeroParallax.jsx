216f8df064ae0cf98d4c401b2501aa20718377e8
chrispark5 <5chrispark5@gmail.com>
2025-03-17 13:45:19 -0500

    Added more sample imagtes

--- nextjs/src/components/HeroParallax.jsx
+++ nextjs/src/components/HeroParallax.jsx
@@ -43,53 +43,50 @@ export const products = [
     link: "https://algochurn.com",
     thumbnail: "/images/products/watch.jpg",
   },
-  //   {
-  //     title: "Aceternity UI",
-  //     link: "https://ui.aceternity.com",
-  //     thumbnail:
-  //       "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
-  //   },
-  //   {
-  //     title: "Tailwind Master Kit",
-  //     link: "https://tailwindmasterkit.com",
-  //     thumbnail:
-  //       "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
-  //   },
-  //   {
-  //     title: "SmartBridge",
-  //     link: "https://smartbridgetech.com",
-  //     thumbnail:
-  //       "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
-  //   },
-  //   {
-  //     title: "Renderwork Studio",
-  //     link: "https://renderwork.studio",
-  //     thumbnail:
-  //       "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
-  //   },
+  {
+    title: "Apple Watch",
+    link: "https://algochurn.com",
+    thumbnail: "/images/products/aplwatch1.jpg",
+  },
+  {
+    title: "Apple Watch",
+    link: "https://algochurn.com",
+    thumbnail: "/images/products/aplwatch2.jpg",
+  },
+  {
+    title: "Apple iPhone",
+    link: "https://algochurn.com",
+    thumbnail: "/images/products/iphone1.jpg",
+  },
+  {
+    title: "Apple iPhone",
+    link: "https://algochurn.com",
+    thumbnail: "/images/products/iphone2.jpg",
+  },
+  {
+    title: "Keychron Keyboard",
+    link: "https://algochurn.com",
+    thumbnail: "/images/products/keyboard.jpg",
+  },
+  {
+    title: "Battlecreek Coffee",
+    link: "https://gomoonbeam.com",
+    thumbnail: "/images/products/battlecreek.jpg",
+  },
+  {
+    title: "Airpods",
+    link: "https://cursor.so",
+    thumbnail: "/images/products/airpods.jpg",
+  },
+  {
+    title: "Nike Shoes",
+    link: "https://userogue.com",
+    thumbnail: "/images/products/domino.jpg",
+  },
 
-  //   {
-  //     title: "Creme Digital",
-  //     link: "https://cremedigital.com",
-  //     thumbnail:
-  //       "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
-  //   },
-  //   {
-  //     title: "Golden Bells Academy",
-  //     link: "https://goldenbellsacademy.com",
-  //     thumbnail:
-  //       "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
-  //   },
-  //   {
-  //     title: "Invoker Labs",
-  //     link: "https://invoker.lol",
-  //     thumbnail:
-  //       "https://aceternity.com/images/products/thumbnails/new/invoker.png",
-  //   },
-  //   {
-  //     title: "E Free Invoice",
-  //     link: "https://efreeinvoice.com",
-  //     thumbnail:
-  //       "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
-  //   },
+  {
+    title: "Drink",
+    link: "https://editorially.org",
+    thumbnail: "/images/products/drink.jpg",
+  },
 ];
